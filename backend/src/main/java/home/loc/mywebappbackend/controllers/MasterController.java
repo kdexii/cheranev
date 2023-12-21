12507cf1d02413fa0a9f47c204af25c037c350a2
Gorila <71722922+kdexii@users.noreply.github.com>
2023-12-21 09:31:36 +0300

    work with service list from db

--- backend/src/main/java/home/loc/mywebappbackend/controllers/MasterController.java
+++ backend/src/main/java/home/loc/mywebappbackend/controllers/MasterController.java
@@ -1,5 +1,6 @@
 package home.loc.mywebappbackend.controllers;
 
+import home.loc.mywebappbackend.database.PostgresQueryExecutor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MasterController {
     @GetMapping("/sayhello")
     public String sayHelloWorld(){
+        PostgresQueryExecutor postgresQueryExecutor = new PostgresQueryExecutor();
+        postgresQueryExecutor.getServiceList();
         return "Hello World!";
     }
 }
