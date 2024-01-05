e0281200797a3a259fd501cd0fb47d651c518469
Gorila <71722922+kdexii@users.noreply.github.com>
2024-01-05 03:45:12 +0300

    new web service for picture backend

---
+++ image-server/src/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func getRoot(w http.ResponseWriter, r *http.Request) {
+}
+func getHello(w http.ResponseWriter, r *http.Request) {
+	warnLog := log.New(os.Stdout, "WARN\t", log.Ldate|log.Ltime)
+	imageName := r.URL.Query().Get("name")
+	stringPathTemplateToImage := "./media/" + imageName + ".png"
+
+	fileBytes, err := ioutil.ReadFile(stringPathTemplateToImage)
+	if err != nil {
+		warnLog.Printf("File with name \"%s\" not found in directory media", imageName)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(fileBytes)
+	return
+}
+
+func main() {
+	http.HandleFunc("/", getRoot)
+	http.HandleFunc("/image", getHello)
+
+	err := http.ListenAndServe(":3333", nil)
+	fmt.Println(err)
+}
