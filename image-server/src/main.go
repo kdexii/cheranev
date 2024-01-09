8b59c8b96b7125de22ef3f314255c1858d03e8ca
Gorila <71722922+kdexii@users.noreply.github.com>
2024-01-10 01:18:16 +0300

    added not found page and new image

--- image-server/src/main.go
+++ image-server/src/main.go
@@ -14,10 +14,19 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	warnLog := log.New(os.Stdout, "WARN\t", log.Ldate|log.Ltime)
 	imageName := r.URL.Query().Get("name")
 	stringPathTemplateToImage := "./media/" + imageName + ".png"
+	stringPathTemplateErrorImage := "./media/not_found.png"
 
 	fileBytes, err := ioutil.ReadFile(stringPathTemplateToImage)
 	if err != nil {
 		warnLog.Printf("File with name \"%s\" not found in directory media", imageName)
+		fileBytes, err := ioutil.ReadFile(stringPathTemplateErrorImage)
+		if err != nil {
+			warnLog.Printf("Error image not found!")
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(fileBytes)
+		return
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "application/octet-stream")
